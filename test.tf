382939273cb0ee841aede659f362b36c7cc1e108
james1989 <jamesmic80@gmail.com>
2022-01-20 01:19:32 +0100

    tarraform format

--- test.tf
+++ test.tf
@@ -1,50 +1,50 @@
 ## Configure the vSphere Provider
 provider "vsphere" {
-    user            = var.vsphere_user
-    password        = var.vsphere_password
-    vsphere_server  = var.vsphere_server
-    allow_unverified_ssl = true
+  user                 = var.vsphere_user
+  password             = var.vsphere_password
+  vsphere_server       = var.vsphere_server
+  allow_unverified_ssl = true
 }
 
 # Create Datacenter
 data "vsphere_datacenter" "dc" {
-    name = "ha-datacenter"
+  name = "ha-datacenter"
 }
 
 # Create datastore
 data "vsphere_datastore" "datastore" {
-    name            = datastore2
-    datacenter_id   = data.vsphere_datacenter.dc.id
+  name          = datastore2
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 # Create resource pool
 data "vsphere_resource_pool" "pool" {
-    name            = "cluster1/Resources"
-    datacenter_id   = data.vsphere_datacenter.dc.id
+  name          = "cluster1/Resources"
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 # Create network
 data "vsphere_network" "mgmt_lan" {
-    name            = "VM_VLAN1"
-    datacenter_id   = data.vsphere_datacenter.dc.id
+  name          = "VM_VLAN1"
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 # Create Virtual Machine
 resource "vsphere_virtual_machine" "testvm" {
-    name = "terraform-testvm"
-    resource_pool_id = data.vsphere_resource_pool.pool.id
-    datastore_id = data.vsphere_datastore.datastore.id
-    num_cpus = 1
-    memory = 1024
-    guest_id = "debian11"
-    network_interface {
-      network_id = data.vsphere_network.mgmt_lan.id
-      adapter_type = "vmxnet3"
-    }
-    disk {
-      size = 5
-      name = "terraform-testvm.vmdk"
-      eagerly_scrub = false
-      thin_provisioned = true
-    }
+  name             = "terraform-testvm"
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  num_cpus         = 1
+  memory           = 1024
+  guest_id         = "debian11"
+  network_interface {
+    network_id   = data.vsphere_network.mgmt_lan.id
+    adapter_type = "vmxnet3"
+  }
+  disk {
+    size             = 5
+    name             = "terraform-testvm.vmdk"
+    eagerly_scrub    = false
+    thin_provisioned = true
+  }
 }
