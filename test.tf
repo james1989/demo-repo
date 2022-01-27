d421af60ec2d7a487f0efb7473c4aa1a43b4ae86
james1989 <jamesmic80@gmail.com>
2022-01-27 18:02:26 +0100

    add vmkernel adapters

--- test.tf
+++ test.tf
@@ -40,7 +40,9 @@ resource "vsphere_host_virtual_switch" "hvs1" {
     name = "vSwitch0"
     mtu = 9000
     host_system_id = data.vsphere_host.h1.id
-    network_adapters = ["vmnic0"]
+    network_adapters = ["vmnic0", "vmnic1"]
+    active_nics  = ["vmnic0"]
+    standby_nics = ["vmnic1"]
 }
 
 # Define port group 1
