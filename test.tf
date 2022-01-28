e9d8a9c51d600169e4cdd286c55a5b22e9caaa7c
james1989 <jamesmic80@gmail.com>
2022-01-28 02:09:09 +0100

    add vmkernel adapters

--- test.tf
+++ test.tf
@@ -36,19 +36,20 @@ data "vsphere_network" "mgmt_lan" {
 }
 
 # Create virtual switch
-resource "vsphere_host_virtual_switch" "hvs1" {
-    name = "vSwitch0"
-    mtu = 9000
-    host_system_id = data.vsphere_host.h1.id
-    network_adapters = ["vmnic0"]
-    active_nics  = ["vmnic0"]
-    standby_nics = ["vmnic0"]
-}
+#resource "vsphere_host_virtual_switch" "hvs1" {
+#    name = "vSwitch0"
+#    mtu = 9000
+#    host_system_id = data.vsphere_host.h1.id
+#    network_adapters = ["vmnic0", "vmnic1"]
+#    active_nics = ["vmnic0"]
+#    standby_nics = ["vmnic1"]
+#}
 
 # Define port group 1
 resource "vsphere_host_port_group" "p1" {
     name = "VMK-iSCSI-vl-${var.iscsi_vlan1}"
-    virtual_switch_name = vsphere_host_virtual_switch.hvs1.name
+    #virtual_switch_name = vsphere_host_virtual_switch.hvs1.name
+    virtual_switch_name = vSwitch0
     host_system_id = data.vsphere_host.h1.id
     vlan_id = var.iscsi_vlan1
 }
@@ -56,7 +57,8 @@ resource "vsphere_host_port_group" "p1" {
 # Define port group 2
 resource "vsphere_host_port_group" "p2" {
     name = "VMK-iSCSI-vl-${var.iscsi_vlan2}"
-    virtual_switch_name = vsphere_host_virtual_switch.hvs1.name
+    #virtual_switch_name = vsphere_host_virtual_switch.hvs1.name
+    virtual_switch_name = vSwitch0
     host_system_id = data.vsphere_host.h1.id
     vlan_id = var.iscsi_vlan2
 }
