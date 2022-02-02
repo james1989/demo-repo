10ad0543ec182f8b4a96323acfb36d845d6a8012
james1989 <jamesmic80@gmail.com>
2022-02-03 00:08:02 +0100

    refresh

--- test.tf
+++ test.tf
@@ -69,9 +69,9 @@ resource "vsphere_vnic" "vmk1" {
   portgroup = vsphere_host_port_group.p1.name
   mtu = 9000
   ipv4 {
-    ip = 10.0.2.16
-    netmask = 255.255.255.0
-    gw = 10.0.2.254
+    ip = "10.0.2.16"
+    netmask = "255.255.255.0"
+    gw = "10.0.2.254"
   }
 } 
 
@@ -81,9 +81,9 @@ resource "vsphere_vnic" "vmk2" {
   portgroup = vsphere_host_port_group.p2.name
   mtu = 9000
   ipv4 {
-    ip = 10.0.2.17
-    netmask = 255.255.255.0
-    gw = 10.0.2.254
+    ip = "10.0.2.17"
+    netmask = "255.255.255.0"
+    gw = "10.0.2.254"
   }
 } 
 
