3ff2fd1775f5a8dedf345fbade6c27f9a76313db
james1989 <jamesmic80@gmail.com>
2022-02-03 00:30:49 +0100

    refresh

--- test.tf
+++ test.tf
@@ -6,7 +6,7 @@ provider "vsphere" {
   allow_unverified_ssl = true
 }
 
-# Create Datacenter
+# Define Datacenter
 data "vsphere_datacenter" "dc" {
   name = "ha-datacenter"
 }
@@ -17,33 +17,33 @@ data "vsphere_host" "h1" {
     datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-# Create datastore
+# Define datastore
 data "vsphere_datastore" "datastore" {
   name          = "datastore2"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-# Create resource pool
+# Define resource pool
 data "vsphere_resource_pool" "pool" {
   name          = "cluster1/Resources"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-# Create network
+# Define network
 data "vsphere_network" "mgmt_lan" {
   name          = "VM Network"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-# Create virtual switch
-#resource "vsphere_host_virtual_switch" "hvs1" {
-#    name = "vSwitch0"
-#    mtu = 9000
-#    host_system_id = data.vsphere_host.h1.id
-#    network_adapters = ["vmnic0", "vmnic1"]
-#    active_nics = ["vmnic0"]
-#    standby_nics = ["vmnic1"]
-#}
+# Define virtual switch
+data "vsphere_host_virtual_switch" "hvs1" {
+   name = "vSwitch0"
+   mtu = 9000
+   host_system_id = data.vsphere_host.h1.id
+   network_adapters = ["vmnic0", "vmnic1"]
+   active_nics = ["vmnic0"]
+   standby_nics = ["vmnic1"]
+}
 
 # Define port group 1
 resource "vsphere_host_port_group" "p1" {
@@ -84,21 +84,21 @@ resource "vsphere_vnic" "vmk2" {
 } 
 
 # Create Virtual Machine
-# resource "vsphere_virtual_machine" "testvm" {
-#   name             = "terraform-testvm"
-#   resource_pool_id = data.vsphere_resource_pool.pool.id
-#   datastore_id     = data.vsphere_datastore.datastore.id
-#   num_cpus         = 1
-#   memory           = 1024
-#   guest_id         = "centos7_64Guest"
-#   network_interface {
-#     network_id   = data.vsphere_network.mgmt_lan.id
-#     adapter_type = "vmxnet3"
-#   }
-#   disk {
-#     size             = 5
-#     label             = "terraform-testvm.vmdk"
-#     eagerly_scrub    = false
-#     thin_provisioned = true
-#   }
-# }
+resource "vsphere_virtual_machine" "testvm" {
+  name             = "terraform-testvm"
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  num_cpus         = 1
+  memory           = 1024
+  guest_id         = "centos7_64Guest"
+  network_interface {
+    network_id   = data.vsphere_network.mgmt_lan.id
+    adapter_type = "vmxnet3"
+  }
+  disk {
+    size             = 5
+    label             = "terraform-testvm.vmdk"
+    eagerly_scrub    = false
+    thin_provisioned = true
+  }
+}
