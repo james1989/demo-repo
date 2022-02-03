c8cd328bc425b4c36b79661f748e8174b497795f
james1989 <jamesmic80@gmail.com>
2022-02-03 10:29:11 +0100

    refresh

--- test.tf
+++ test.tf
@@ -35,70 +35,70 @@ data "vsphere_network" "mgmt_lan" {
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-# # Define virtual switch
-# resource "vsphere_host_virtual_switch" "hvs1" {
-#    name = "vSwitch0"
-#    mtu = 9000
-#    host_system_id = data.vsphere_host.h1.id
-#    network_adapters = ["vmnic0", "vmnic1"]
-#    active_nics = ["vmnic0"]
-#    standby_nics = ["vmnic1"]
+# Define virtual switch
+resource "vsphere_host_virtual_switch" "hvs1" {
+   name = "vSwitch0"
+   mtu = 9000
+   host_system_id = data.vsphere_host.h1.id
+   network_adapters = ["vmnic0", "vmnic1"]
+   active_nics = ["vmnic0"]
+   standby_nics = ["vmnic1"]
+}
+
+# # Define port group 1
+# resource "vsphere_host_port_group" "p1" {
+#     name = "VMK-iSCSI-vl-${var.iscsi_vlan1}"
+#     #virtual_switch_name = vsphere_host_virtual_switch.hvs1.name
+#     virtual_switch_name = "vSwitch0"
+#     host_system_id = data.vsphere_host.h1.id
+#     vlan_id = var.iscsi_vlan1
 # }
 
-# Define port group 1
-resource "vsphere_host_port_group" "p1" {
-    name = "VMK-iSCSI-vl-${var.iscsi_vlan1}"
-    #virtual_switch_name = vsphere_host_virtual_switch.hvs1.name
-    virtual_switch_name = "vSwitch0"
-    host_system_id = data.vsphere_host.h1.id
-    vlan_id = var.iscsi_vlan1
-}
+# # Define port group 2
+# resource "vsphere_host_port_group" "p2" {
+#     name = "VMK-iSCSI-vl-${var.iscsi_vlan2}"
+#     #virtual_switch_name = vsphere_host_virtual_switch.hvs1.name
+#     virtual_switch_name = "vSwitch0"
+#     host_system_id = data.vsphere_host.h1.id
+#     vlan_id = var.iscsi_vlan2
+# }
 
-# Define port group 2
-resource "vsphere_host_port_group" "p2" {
-    name = "VMK-iSCSI-vl-${var.iscsi_vlan2}"
-    #virtual_switch_name = vsphere_host_virtual_switch.hvs1.name
-    virtual_switch_name = "vSwitch0"
-    host_system_id = data.vsphere_host.h1.id
-    vlan_id = var.iscsi_vlan2
-}
+# # Define vmkernel adapter1
+# resource "vsphere_vnic" "vmk1" {
+#   host = data.vsphere_host.h1.id
+#   portgroup = vsphere_host_port_group.p1.name
+#   mtu = 9000
+#   ipv4 {
+#     dhcp = true
+#   }
+# } 
 
-# Define vmkernel adapter1
-resource "vsphere_vnic" "vmk1" {
-  host = data.vsphere_host.h1.id
-  portgroup = vsphere_host_port_group.p1.name
-  mtu = 9000
-  ipv4 {
-    dhcp = true
-  }
-} 
+# # Define vmkernel adapter2
+# resource "vsphere_vnic" "vmk2" {
+#   host = data.vsphere_host.h1.id
+#   portgroup = vsphere_host_port_group.p2.name
+#   mtu = 9000
+#   ipv4 {
+#     dhcp = true
+#   }
+# } 
 
-# Define vmkernel adapter2
-resource "vsphere_vnic" "vmk2" {
-  host = data.vsphere_host.h1.id
-  portgroup = vsphere_host_port_group.p2.name
-  mtu = 9000
-  ipv4 {
-    dhcp = true
-  }
-} 
-
-# Create Virtual Machine
-resource "vsphere_virtual_machine" "testvm" {
-  name             = "terraform-testvm"
-  resource_pool_id = data.vsphere_resource_pool.pool.id
-  datastore_id     = data.vsphere_datastore.datastore.id
-  num_cpus         = 1
-  memory           = 1024
-  guest_id         = "centos7_64Guest"
-  network_interface {
-    network_id   = data.vsphere_network.mgmt_lan.id
-    adapter_type = "vmxnet3"
-  }
-  disk {
-    size             = 5
-    label             = "terraform-testvm.vmdk"
-    eagerly_scrub    = false
-    thin_provisioned = true
-  }
-}
+# # Create Virtual Machine
+# resource "vsphere_virtual_machine" "testvm" {
+#   name             = "terraform-testvm"
+#   resource_pool_id = data.vsphere_resource_pool.pool.id
+#   datastore_id     = data.vsphere_datastore.datastore.id
+#   num_cpus         = 1
+#   memory           = 1024
+#   guest_id         = "centos7_64Guest"
+#   network_interface {
+#     network_id   = data.vsphere_network.mgmt_lan.id
+#     adapter_type = "vmxnet3"
+#   }
+#   disk {
+#     size             = 5
+#     label             = "terraform-testvm.vmdk"
+#     eagerly_scrub    = false
+#     thin_provisioned = true
+#   }
+# }
