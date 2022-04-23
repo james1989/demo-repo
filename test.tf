bdb159cfa66fc326ab6dc3449134760801821b03
james1989 <jamesmic80@gmail.com>
2022-04-23 19:12:01 +0200

    imported all infrastructure changes to tf

--- test.tf
+++ test.tf
@@ -23,80 +23,95 @@ data "vsphere_datastore" "datastore" {
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-# Define resource pool
-data "vsphere_resource_pool" "pool" {
-  name          = "cluster1/Resources"
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-# Define network
-data "vsphere_network" "mgmt_lan" {
-  name          = "VM Network"
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
+# # Define resource pool
+# data "vsphere_resource_pool" "pool" {
+#   name          = "cluster1/Resources"
+#   datacenter_id = data.vsphere_datacenter.dc.id
+# }
 
 # Define virtual switch
-resource "vsphere_host_virtual_switch" "hvs1" {
+resource "vsphere_host_virtual_switch" "vSwitch0" {
    name = "vSwitch0"
    mtu = 9000
    host_system_id = data.vsphere_host.h1.id
-   network_adapters = ["vmnic0", "vmnic1"]
+   network_adapters = ["vmnic1", "vmnic0"]
    active_nics = ["vmnic0"]
    standby_nics = ["vmnic1"]
 }
 
 # Define port group 1
-resource "vsphere_host_port_group" "p1" {
-    name = "VMK-iSCSI-vl-${var.iscsi_vlan1}"
-    virtual_switch_name = vsphere_host_virtual_switch.hvs1.name
+# resource "vsphere_host_port_group" "p1" {
+#     name = "VMK-iSCSI-vl-${var.iscsi_vlan1}"
+#     virtual_switch_name = vsphere_host_virtual_switch.hvs1.name
+#     host_system_id = data.vsphere_host.h1.id
+#     vlan_id = var.iscsi_vlan1
+# }
+
+# # Define port group 2
+# resource "vsphere_host_port_group" "p2" {
+#     name = "VMK-iSCSI-vl-${var.iscsi_vlan2}"
+#     virtual_switch_name = vsphere_host_virtual_switch.hvs1.name
+#     host_system_id = data.vsphere_host.h1.id
+#     vlan_id = var.iscsi_vlan2
+# }
+
+resource "vsphere_host_port_group" "vm_network" {
+    name = "VM Network"
+    virtual_switch_name = vsphere_host_virtual_switch.vSwitch0.name
     host_system_id = data.vsphere_host.h1.id
-    vlan_id = var.iscsi_vlan1
+    vlan_id = 0
 }
 
-# Define port group 2
-resource "vsphere_host_port_group" "p2" {
-    name = "VMK-iSCSI-vl-${var.iscsi_vlan2}"
-    virtual_switch_name = vsphere_host_virtual_switch.hvs1.name
+resource "vsphere_host_port_group" "mgmt_network" {
+    name = "Management Network"
+    virtual_switch_name = vsphere_host_virtual_switch.vSwitch0.name
     host_system_id = data.vsphere_host.h1.id
-    vlan_id = var.iscsi_vlan2
+    vlan_id = 0
+    active_nics  = ["vmnic0"]
+    standby_nics = ["vmnic1"]
+    failback = true
+    notify_switches = true
+    teaming_policy = "loadbalance_srcid"
 }
 
 # Define vmkernel adapter1
-resource "vsphere_vnic" "vmk1" {
+resource "vsphere_vnic" "vmk0" {
   host = data.vsphere_host.h1.id
-  portgroup = vsphere_host_port_group.p1.name
+  portgroup = vsphere_host_port_group.mgmt_network.name
   mtu = 9000
+  netstack = "defaultTcpipStack"
   ipv4 {
     dhcp = true
   }
 } 
 
 # Define vmkernel adapter2
-resource "vsphere_vnic" "vmk2" {
+resource "vsphere_vnic" "vmk1" {
   host = data.vsphere_host.h1.id
-  portgroup = vsphere_host_port_group.p2.name
+  portgroup = vsphere_host_port_group.vm_network.name
   mtu = 9000
+  netstack = "defaultTcpipStack"
   ipv4 {
     dhcp = true
   }
 } 
 
 # Create Virtual Machine
-resource "vsphere_virtual_machine" "testvm" {
-  name             = "terraform-testvm"
-  resource_pool_id = data.vsphere_resource_pool.pool.id
-  datastore_id     = data.vsphere_datastore.datastore.id
-  num_cpus         = 1
-  memory           = 1024
-  guest_id         = "centos7_64Guest"
-  network_interface {
-    network_id   = data.vsphere_network.mgmt_lan.id
-    adapter_type = "vmxnet3"
-  }
-  disk {
-    size             = 5
-    label             = "terraform-testvm.vmdk"
-    eagerly_scrub    = false
-    thin_provisioned = true
-  }
-}
+# resource "vsphere_virtual_machine" "testvm" {
+#   name             = "terraform-testvm"
+#   resource_pool_id = data.vsphere_resource_pool.pool.id
+#   datastore_id     = data.vsphere_datastore.datastore.id
+#   num_cpus         = 1
+#   memory           = 1024
+#   guest_id         = "centos7_64Guest"
+#   network_interface {
+#     network_id   = data.vsphere_network.mgmt_lan.id
+#     adapter_type = "vmxnet3"
+#   }
+#   disk {
+#     size             = 5
+#     label             = "terraform-testvm.vmdk"
+#     eagerly_scrub    = false
+#     thin_provisioned = true
+#   }
+# }
